6d3e93b94dbb0c010311451e50069facaec1539b
Md Abu Alam <abualam@outlook.com>
2020-08-08 18:35:33 +0600

    advanced javascript learning

--- object-property.js
+++ object-property.js
@@ -1,15 +1,20 @@
-const students = [
-    {id: 21, name: 'Omar Sunny'},
-    {id: 31, name: 'Maannaaaaaaa'},
-    {id: 41, name: 'Moyouri'},
-    {id: 71, name: 'DeepJol'},
-];
+// const students = [
+//     {id: 21, name: 'Omar Sunny'},
+//     {id: 31, name: 'Maannaaaaaaa'},
+//     {id: 41, name: 'Moyouri'},
+//     {id: 71, name: 'DeepJol'},
+// ];
 
-const names = students.map(s => s.name);
-const ids = students.map (s => s.id);
+// const names = students.map(s => s.name);
+// const ids = students.map (s => s.id);
 
-const older = students.filter(s => s.id > 40);
-const bigger = students.find( s => s.id > 40);
+// const older = students.filter(s => s.id > 40);
+// const bigger = students.find( s => s.id > 40);
 
-console.log(bigger);
+// console.log(bigger);
 
+function doSomething (x, y=4) {
+    console.log(y);
+}
+
+doSomething(3, 2);
